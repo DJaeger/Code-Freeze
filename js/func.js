93cf26fd2c6264caff7285ea9a3a4fe2d7ca50b5
Daniel Jäger <daniel-jaeger@online.de>
2016-05-31 18:08:33 +0200

    Added license info to func.js

--- js/func.js
+++ js/func.js
@@ -1,7 +1,31 @@
 /*
  * @package  code-freeze
  */
- 
+
+/**
+ * LICENSE
+ * This file is part of Code Freeze.
+ * 
+ * Code Freeze is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * Code Freeze is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with Code Freeze.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * @package    code-freeze
+ * @author     Daniel Jäger <mail@djaeger.info>
+ * @copyright  Copyright 2016 Daniel Jäger
+ * @license    http://www.gnu.org/licenses/gpl-3.0.txt GPL 3.0
+ * @link       http://wordpress.org/extend/plugins/code-freeze/
+ */
+
 (function ($) {
 
     $.fn.filterByData = function (prop, val) {
